e32394391edc0369606a4be64cf2c9a824e7eb3c
Fede Alonso <fede488@gmail.com>
2023-04-28 08:11:30 +0000

    modularize

--- gpt_telegram_bot.py
+++ gpt_telegram_bot.py
@@ -15,49 +15,53 @@ from telegram.ext import (
 )
 
 
-# Enable logging
+# Load config and configure logging
+CONFIG_FILE = "resources/config.json"
+CONFIG = None
+with open(CONFIG_FILE) as f:
+        CONFIG = json.loads(f.read())
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+#TODO: Sacar el config Aquí y el output y todo
 
-CONFIG_FILE = "resources/config.json"
+
+
 NOT_ALLOWED_USER, CHAT = range(2)
-
-PROMPT = prompt=f"""
-    Eres el asistente virtual de la empresa Equisalud. Eres amable, educado y predispuesto a ayudar. Únicamente resolverás dudas relacionadas con Equisalud. Si alguien te hace una pregunta de la que desconoces la respuesta, redirígele a la web de contacto (https://www.equisalud.com/es-es/contacto/) y al número de atención al cliente 919 49 82 56. 
-    Qué es equisalud: Equisalud es laboratorio de referencia en fitoterapia, complementos alimenticios y cosmética natural. Con sede en Huarte, Pamplona, inició su andadura en 1989 impulsado por el farmacéutico y biólogo Antonio Vega, quien combina su vasto conocimiento y experiencia profesional con una inmensa afición personal por la recolección y clasificación de plantas medicinales y por la investigación de sus usos tradicionales. Equisalud tiene como misión investigar, desarrollar y fabricar preparados naturales de altísima eficacia, diseñados para mantener y restaurar la salud, respetando los procesos fisiológicos del cuerpo. Nuestro método de trabajo está basado en el respeto a la naturaleza, en el detallado estudio de los principios activos y en los controles de calidad exhaustivos. Equisalud elabora complementos alimenticios con formulaciones propias basadas en principios activos naturales y en ingredientes de la más alta calidad, de procedencia garantizada y de gran eficacia. Cada complemento ha sido diseñado para aportar salud y bienestar. Muchas de las formulaciones parten de considerar a la persona de manera integral como un todo, teniendo en cuenta los planos físico, emocional, mental y espiritual. Estas formulaciones alcanzan su máxima eficiencia a través de un sistema de fabricación patentado por Equisalud. Con sus más de 400 formulaciones, que se encuentran al alcance de los consumidores en más de 20 países, Equisalud es laboratorio de referencia de complementos alimenticios para los profesionales de la salud, productos que son frecuentemente recomendados por médicos, nutricionistas, odontólogos, enfermeras, farmacéuticos, naturópatas, homeópatas, sintergéticos, kinesiólogos, osteópatas, quiroprácticos, acupuntores y profesionales de la medicina tradicional china, medicina ayurvédica, medicina antroposófica y medicina integrativa.
-    La web de Equisalud es: https://www.equisalud.com
-    La web donde encontrar información acerca de los productos, e incluso comprarlos, es: https://www.equisalud.com/es-es/productos/
-    La web donde encontrar información sobre los componentes de los productos es https://www.equisalud.com/es-es/componentes/
-    """
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
     Entry point
     """
-
-    # Load config:
-    config = None
-    with open(CONFIG_FILE) as f:
-        config = json.loads(f.read())
-
-    context.user_data["config"] = config
+    output_folder = CONFIG.get("output_folder")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     user = update.message.from_user
+    # Set a log file per user
+    logging.basicConfig(filename = f'{os.path.join(output_folder, str(user["id"]))}.log')
+    
     logger.info(f"Started conversation from user: '{user['first_name']}' with username '{user['username']}' and id '{user['id']}'")
+    context.user_data["user"] = user
 
-    if update.message.from_user.id not in config.get("allowed_users", []):
+    if update.message.from_user.id not in CONFIG.get("allowed_users", []):
         logger.info(f"User '{user['id']}' not allowed!")
         await update.message.reply_text(f"User '{user['id']}' not allowed!", reply_markup=ReplyKeyboardRemove())
         return NOT_ALLOWED_USER
-    
-    user = update.message.from_user.first_name.upper()
-    greetings = config.get("texts").get("greetings").format(user)
+        
+    greetings = CONFIG.get("texts").get("greetings").format(user['first_name'])
     
     # Load the initial prompt
-    context.user_data["prompt"] = PROMPT + "\n" + " AI: "+ greetings
+    with open(CONFIG.get("prompt_file_path"), 'r')as f:
+        prompt = f.read()
+    
+    logger.info(f"{user['id']} - Prompt: {prompt}")
+    logger.info(f"{user['id']} - Greeting message: {greetings}")
+
+    # Add the Greetings message to the prompt
+    context.user_data["prompt"] = prompt + "\n" + f" {CONFIG.get('gpt_config').get('stop')[1]}: "+ greetings
 
     await update.message.reply_text(greetings, reply_markup=ReplyKeyboardRemove())
     return CHAT
@@ -67,40 +71,45 @@ async def receive_not_allowed_user(update: Update, context: ContextTypes.DEFAULT
     """
     Filter allowed users
     """
-
-    config = context.user_data.get("config")
     #TODO: This is not working properly. It filters the users but don't write the message
-    await update.message.reply_text(f"Holassss {user}, soy tu asistente personal. Mucha IA. En qué puedo ayudarte?", reply_markup=ReplyKeyboardRemove())
+    await update.message.reply_text(CONFIG.get("texts").get("not_allowed"), reply_markup=ReplyKeyboardRemove())
     return NOT_ALLOWED_USER
     
 
-async def receive_expense_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def gpt_chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
-    Get the Expense Description
+    Chat with GPT
     """
-
-    config = context.user_data.get("config")
+    # Get customer message
     customer_text = update.message.text
-    context.user_data["prompt"] = context.user_data["prompt"] + "\n" + " Human: "+ customer_text + "\n" + " AI: "
+    logger.info(f"{context.user_data.get('user')['id']} - Customer Chat message: {customer_text}")
+
+    # Add customer message to the prompt
+    gpt_config = CONFIG.get("gpt_config")
+    context.user_data["prompt"] = context.user_data["prompt"] + "\n" + f" {gpt_config.get('stop')[0]}: "+ customer_text + "\n" + f" {gpt_config.get('stop')[1]}: "
     
+    # Configure GPT engine
     openai.api_key = os.getenv("OPENAI_API_KEY")
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model=gpt_config.get("model"),
         prompt=context.user_data["prompt"],
-        temperature=0.1,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"]
+        temperature=gpt_config.get("temperature"),
+        max_tokens=gpt_config.get("max_tokens"),
+        top_p=gpt_config.get("top_p"),
+        frequency_penalty=gpt_config.get("frequency_penalty"),
+        presence_penalty=gpt_config.get("presence_penalty"),
+        stop=gpt_config.get("stop")
     )
 
+    # Get GPT response
     response_text = unidecode(response.get("choices")[0].get("text"))
+    logger.info(f"{context.user_data.get('user')['id']} - GPT Usage: {json.dumps(response.get('usage'))} - GPT Response: {response_text}")
 
+    # Add GPT response to the prompt
     context.user_data["prompt"] = context.user_data["prompt"] + response_text
 
+    # Wait for customer message and continue chatting
     await update.message.reply_text(response_text)
-
     return CHAT
 
 
@@ -108,13 +117,11 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """
     Cancel and ends the conversation.
     """
-    
-    config = context.user_data.get("config")
 
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     await update.message.reply_text(
-        config.get("texts").get("restart_text"), reply_markup=ReplyKeyboardRemove()
+        CONFIG.get("texts").get("restart_text"), reply_markup=ReplyKeyboardRemove()
     )
     return ConversationHandler.END
 
@@ -132,7 +139,7 @@ def main() -> None:
         entry_points=[CommandHandler("start", start)],
         states={
             NOT_ALLOWED_USER:[MessageHandler(filters.TEXT & ~filters.COMMAND, receive_not_allowed_user)],
-            CHAT: [MessageHandler(filters.TEXT & ~filters.COMMAND, receive_expense_description)]
+            CHAT: [MessageHandler(filters.TEXT & ~filters.COMMAND, gpt_chat)]
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
